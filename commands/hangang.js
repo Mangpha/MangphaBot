a593b5ca41cf0975a84db6920adcb3bc56f67634
Mangpha <8350130@gmail.com>
2022-02-26 12:40:14 +0900

    #5. Add HanGang Command

--- commands/hangang.js
+++ commands/hangang.js
@@ -1,8 +1,17 @@
+const axios = require("axios");
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder().setName("한강").setDescription("한강 물 온도 확인"),
     async execute(interaction) {
-        await interaction.reply("한강 물 온도 확인 기능");
+        const res = await axios.get("https://api.hangang.msub.kr");
+        const embed = new MessageEmbed()
+            .setColor("#0099ff")
+            .setTitle("한강 물 온도")
+            .setAuthor({ name: "Mangpha Bot" })
+            .setDescription(`현재 온도는 ${res.data.temp}도 입니다.`)
+            .setTimestamp();
+        await interaction.reply({ embeds: [embed] });
     },
 };
