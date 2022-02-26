f97afc3f24856e97089ceea53b114726bb8e765a
Mangpha <8350130@gmail.com>
2022-02-26 12:24:12 +0900

    #4. Testing Embed Message

--- commands/test.js
+++ commands/test.js
@@ -1,8 +1,16 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+
+const embed = new MessageEmbed()
+    .setColor("#0099ff")
+    .setTitle("Testing")
+    .setAuthor({ name: "Mangpha Bot" })
+    .setDescription("Testing Embed Message")
+    .setTimestamp();
 
 module.exports = {
     data: new SlashCommandBuilder().setName("test").setDescription("Testing Slash Command"),
     async execute(interaction) {
-        await interaction.reply("Testing Bot");
+        await interaction.reply({ embeds: [embed] });
     },
 };
