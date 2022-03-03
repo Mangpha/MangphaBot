b6d63329edc71a0eca7cb1741d0e27f6fdd1ddb4
Mangpha <8350130@gmail.com>
2022-03-04 03:33:27 +0900

    #8. Bracelet Command Setting

--- commands/bracelet.js
+++ commands/bracelet.js
@@ -2,4 +2,13 @@ const axios = require("axios");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageEmbed } = require("discord.js");
 
-module.exports = {};
+module.exports = {
+    data: new SlashCommandBuilder().setName("팔찌").setDescription("팔찌 시뮬레이터"),
+    async execute(interaction) {
+        const embed = new MessageEmbed()
+            .setColor("#0c0c0c")
+            .setTitle("")
+            .setAuthor({ name: "Mangpha Bot" });
+        await interaction.reply({ embeds: embed });
+    },
+};
